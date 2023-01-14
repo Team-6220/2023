30f488decf96eec6eff1f173184aafad89b57413
DanielWu06 <danielsthings1@gmail.com>
2023-01-14 08:29:52 -0800

    Update AutoClimbCommand.java

--- src/main/java/frc/robot/commands/AutoClimbCommand.java
+++ src/main/java/frc/robot/commands/AutoClimbCommand.java
@@ -3,34 +3,17 @@ package frc.robot.commands;
 //import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj2.command.*;
 
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
-public class AutoClimbCommand extends CommandBase {
+public class AutoClimbCommand extends SequentialCommandGroup {
     //private double runtime;
     private final DrivetrainSubsystem m_drivetrainSubsystem;
-    private double speed;
 
-
-    //@Param
-    //runtime is time to run
-    //speed is motor velocity in m/s
-    //runtime * speed should equal 0.99 or 1 meter
-
-    public AutoClimbCommand(double runtime, DrivetrainSubsystem drivetrainSubsystem, double speed){
-        //super(runtime);
+    public AutoClimbCommand(double runtime, DrivetrainSubsystem drivetrainSubsystem){
         this.m_drivetrainSubsystem = drivetrainSubsystem;
-        this.speed = speed;
-    }
 
-    @Override
-    public void initialize(){
-        m_drivetrainSubsystem.drive(new ChassisSpeeds(speed,0,0));
-    }
+        new DriveDistanceCommand(m_drivetrainSubsystem, 1);
 
-    @Override
-    public void end(boolean interrupted){
-        m_drivetrainSubsystem.drive(new ChassisSpeeds(0,0,0));
-        m_drivetrainSubsystem.lockWheels();
+        new LockWheels(m_drivetrainSubsystem);
     }
 }
