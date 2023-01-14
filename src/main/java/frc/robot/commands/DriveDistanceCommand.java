88201417621ac1439ccb04d7b81809317a47bd29
DanielWu06 <danielsthings1@gmail.com>
2023-01-14 08:22:18 -0800

    Update DriveDistanceCommand.java

--- src/main/java/frc/robot/commands/DriveDistanceCommand.java
+++ src/main/java/frc/robot/commands/DriveDistanceCommand.java
@@ -2,15 +2,34 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class DriveDistanceCommand extends CommandBase{
     private final DrivetrainSubsystem m_drivetrainSubsystem;
     private final double goal;
+    private final Translation2d init_t;
+    
 
     public DriveDistanceCommand(DrivetrainSubsystem drivetrainSubsystem, double goal){
         this.m_drivetrainSubsystem = drivetrainSubsystem;
         this.goal = goal;
+        init_t = m_drivetrainSubsystem.getPose().getTranslation();
     }
 
-    //TODO: IMPLEMENT DRIVING FOR GOAL METERS
+    @Override
+    public void execute(){
+        Pose2d cur = m_drivetrainSubsystem.getPose();
+        if(Math.abs(cur.getTranslation().getDistance(init_t)) < goal){
+            m_drivetrainSubsystem.drive(new ChassisSpeeds(0.5,0,0));
+        }else{
+            end(false);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        m_drivetrainSubsystem.drive(new ChassisSpeeds(0,0,0));
+    }
 }
